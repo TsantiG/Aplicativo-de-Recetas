d7e87ab925116480584a9e76dc8c9aced9336612
TsantiG <santigutierrez081@gmail.com>
2024-09-09 22:34:19 -0500

    comienzo, comprobacion de funcionalidad

---
+++ Final_FronEnd/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <header class="py-8 px-4 mx-auto max-w-xl lg:py-16 lg:px-6">
+            <nav>
+                <h1>Hola</h1>
+            </nav>
+        </header>
+    );
+};
+
+
+
+
+
+export default MyComponent;
