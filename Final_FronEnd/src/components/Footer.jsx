aee3219438b60ce1617ce617e89320453a97b5d1
TsantiG <santigutierrez081@gmail.com>
2024-09-11 01:54:20 -0500

    Creacion de las paginas necesarias, creacion de los componentes header para la nav y el footer (Contenido por definir)

---
+++ Final_FronEnd/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <p></p>
+            </div>
+            <div>
+                <p></p>
+            </div>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+
+
+
+
+export default Footer;
--- Final_FronEnd/src/components/Header.jsx
+++ Final_FronEnd/src/components/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const MyComponent = () => {
+const Header = () => {
     return (
-        <header class="py-8 px-4 mx-auto max-w-xl lg:py-16 lg:px-6">
-            <nav>
-                <h1>Hola</h1>
+        <header className="py-8 px-4 mx-auto max-w-xl lg:py-16 lg:px-6">
+            <nav className="navbar">
+                <div className="logo">
+                    <a href="#"><img src="logo.png" alt="Logo" /></a>
+                </div>
+                <div className="nav-links">
+                    <a href="#recetas">Mis Recetas</a>
+                    <a href="#compartir">Compartir Recetas</a>
+                    <a href="#social">Probar Algo Nuevo</a>
+                </div>
+                <div className="user-info">
+                    <img src="profile.jpg" alt="Foto de perfil" /> 
+                    <span>Nombre del Usuario</span>
+                    <button className="logout-btn">Cerrar Sesión</button>
+                </div>
             </nav>
         </header>
     );
@@ -14,4 +26,4 @@ const MyComponent = () => {
 
 
 
-export default MyComponent;
+export default Header;
