a9d490a0b54a36504df41dcf4d4352e929f0864e
TsantiG <santigutierrez081@gmail.com>
2024-09-11 12:40:50 -0500

    agregacion de algunas imagenes por medio de claudinary

--- Final_FronEnd/src/components/Header.jsx
+++ Final_FronEnd/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Header = () => {
+    const imgUrl='https://res.cloudinary.com/dstpvt64c/image/upload/v1726074707/logo_1_qereih.png';
     return (
-            <header className="py-4 px-10 flex items-center fixed top-7 w-full justify-between bg-rosa"/* mx-auto max-w-xl lg:py-16 lg:px-6 */>
-            <div className="flex flex-grow ">
-                        <a href="#"><img className='h-6 w-32' src="logo.png" alt="Logo" /*fill="currentColor"*/ /></a>
-                    </div>
+        <header className="py-4 px-10 flex items-center fixed top-7 w-full justify-between bg-rosa z-10">
+            <div className="flex flex-grow">
+            <a href="#"></a>
+            </div>
                 <nav>
                     <ul className='flex text-2xl [&>li>a]:inline-block [&>li>a]:px-6 [&>li>a]:py-1 [&>li>a]:mx-12 [&>li>a]:bg-white [&>li>a]:rounded-xl [&>li>a]:border-black [&>li>a]:border-2'>
                         <li><a href="#recetas">Mis Recetas</a></li>
