34621a0c52032a6b07d48fde9775a61db678d1d6
TsantiG <santigutierrez081@gmail.com>
2024-09-11 11:10:09 -0500

    estructuracion del nav y del footer (contenido por revisar)

--- Final_FronEnd/src/components/Footer.jsx
+++ Final_FronEnd/src/components/Footer.jsx
@@ -2,25 +2,17 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            <div>
-                <p></p>
-            </div>
-            <div>
-                <p></p>
-            </div>
-            <div>
-                <div>
-                    <img src="" alt="" />
-                </div>
-                <div>
-                    <img src="" alt="" />
-                </div>
-                <div>
-                    <img src="" alt="" />
-                </div>
-            </div>
-        </footer>
+    <footer class="text-black text-xl  rounded-lg shadow m-4 bg-rosa mt-auto">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span class="  sm:text-center ">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <ul class="flex flex-wrap items-center mt-3  font-medium  sm:mt-0">
+            <li><a href="#" class="hover:underline me-4 md:me-6">About</a></li>
+            <li><a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a></li>
+            <li><a href="#" class="hover:underline me-4 md:me-6">Licensing</a></li>
+            <li><a href="#" class="hover:underline">Contact</a></li>
+        </ul>
+        </div>
+    </footer>
     );
 };
 
--- Final_FronEnd/src/components/Header.jsx
+++ Final_FronEnd/src/components/Header.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header className="py-8 px-4 mx-auto max-w-xl lg:py-16 lg:px-6">
-            <nav className="navbar">
-                <div className="logo">
-                    <a href="#"><img src="logo.png" alt="Logo" /></a>
-                </div>
-                <div className="nav-links">
-                    <a href="#recetas">Mis Recetas</a>
-                    <a href="#compartir">Compartir Recetas</a>
-                    <a href="#social">Probar Algo Nuevo</a>
-                </div>
-                <div className="user-info">
-                    <img src="profile.jpg" alt="Foto de perfil" /> 
-                    <span>Nombre del Usuario</span>
-                    <button className="logout-btn">Cerrar Sesión</button>
-                </div>
-            </nav>
-        </header>
+            <header className="py-4 px-10 flex items-center fixed top-7 w-full justify-between bg-rosa"/* mx-auto max-w-xl lg:py-16 lg:px-6 */>
+            <div className="flex flex-grow ">
+                        <a href="#"><img className='h-6 w-32' src="logo.png" alt="Logo" /*fill="currentColor"*/ /></a>
+                    </div>
+                <nav>
+                    <ul className='flex text-2xl [&>li>a]:inline-block [&>li>a]:px-6 [&>li>a]:py-1 [&>li>a]:mx-12 [&>li>a]:bg-white [&>li>a]:rounded-xl [&>li>a]:border-black [&>li>a]:border-2'>
+                        <li><a href="#recetas">Mis Recetas</a></li>
+                        <li><a href="#compartir">Compartir Recetas</a></li>
+                        <li><a href="#social">Probar Algo Nuevo</a></li>
+                    </ul>
+                </nav>
+                <nav className='flex flex-grow justify-end'>
+                    <div className="flex">
+                        <img className='inline-block px-1 py-2' src="profile.jpg" alt="Foto de perfil" /> 
+                        <span className='text-xl  inline-block px-1 py-2'>Nombre del Usuario</span>
+                        <button className="bg-red-800 text-white rounded-lg inline-block px-1 py-2 text-2xl justify-end">Cerrar Sesión</button>
+                    </div>
+                </nav>
+            </header>
     );
 };
 
